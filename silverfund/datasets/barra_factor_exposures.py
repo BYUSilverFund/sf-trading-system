c0b4a67d6bd6940d28fc75a48da22ca2c60ece22
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-01-14 21:52:30 -0700

    Cleaned up code

--- silverfund/datasets/barra_factor_exposures.py
+++ silverfund/datasets/barra_factor_exposures.py
@@ -1,14 +1,17 @@
-from silverfund.datasets.dataset import Dataset
-from datetime import date
-from typing import Optional
-import polars as pl
 import os
-from dotenv import load_dotenv
+from datetime import date
 from pathlib import Path
-from tqdm import tqdm
-from silverfund.database import Database
+from typing import Optional
 
-class BarraFactorExposures():
+import polars as pl
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+from silverfund.database import Database
+from silverfund.datasets.dataset import Dataset
+
+
+class BarraFactorExposures:
 
     def __init__(self) -> None:
         self.db = Database()
@@ -17,7 +20,7 @@ class BarraFactorExposures():
 
         user = os.getenv("ROOT").split("/")[2]
         root_dir = Path(f"/home/{user}")
-        
+
         self._folder = root_dir / "groups" / "grp_quant" / "data" / "barra_usslow"
         self._files = os.listdir(self._folder)
 
@@ -28,7 +31,7 @@ class BarraFactorExposures():
         for year in tqdm(years, desc="Downloading parquet files"):
             df = pl.read_parquet(self._folder / f"exposures_{year}.parquet")
             dfs.append(self.clean(df))
-        
+
         result = pl.concat(dfs)
 
         self.db.create("BARRA_FACTOR_EXPOSURES", result)
@@ -36,7 +39,7 @@ class BarraFactorExposures():
     def load(self) -> pl.DataFrame:
 
         return self.db.read("BARRA_FACTOR_EXPOSURES")
-    
+
     @staticmethod
     def clean(df: pl.DataFrame) -> pl.DataFrame:
 
@@ -45,26 +48,25 @@ class BarraFactorExposures():
         df = df.rename({col: new_col for col, new_col in zip(df.columns, new_cols)})
 
         # Split Combined colum into Barrid and Factor
-        df = df.with_columns(
-            pl.col('Combined').str.split("/").alias("parts")
-        ).with_columns(
-            pl.col('parts').list.first().alias('Barrid'),
-            pl.col('parts').list.last().alias('Factor')
-        ).drop(['Combined', 'parts'])
-
-        # Melt date headers into a column
-        df = df.unpivot(
-            index=["Barrid", "Factor"],
-            variable_name="Date",
-            value_name="Value"
+        df = (
+            df.with_columns(pl.col("Combined").str.split("/").alias("parts"))
+            .with_columns(
+                pl.col("parts").list.first().alias("Barrid"),
+                pl.col("parts").list.last().alias("Factor"),
+            )
+            .drop(["Combined", "parts"])
         )
 
+        # Melt date headers into a column
+        df = df.unpivot(index=["Barrid", "Factor"], variable_name="Date", value_name="Value")
+
         # Sort
-        df = df.sort(by=['Barrid', 'Date'])
+        df = df.sort(by=["Barrid", "Date"])
 
         return df
-    
+
+
 if __name__ == "__main__":
     bf = BarraFactorExposures()
     bf.download()
-    print(bf.load())    
+    print(bf.load())
