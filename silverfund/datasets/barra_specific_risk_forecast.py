ea6b5e69e10737257982c92970b3829e94f85c7f
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-01-15 11:27:58 -0700

    New datasets

--- silverfund/datasets/barra_specific_risk_forecast.py
+++ silverfund/datasets/barra_specific_risk_forecast.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from silverfund.database import Database
 
 
-class BarraFactorExposures:
+class BarraSpecificRiskForecast:
 
     def __init__(self) -> None:
         self.db = Database()
@@ -22,7 +22,7 @@ class BarraFactorExposures:
 
     def load(self, year: int) -> pl.DataFrame:
 
-        file = f"exposures_{year}.parquet"
+        file = f"spec_risk_{year}.parquet"
 
         return self.clean(pl.read_parquet(self._folder / file))
 
@@ -31,8 +31,8 @@ class BarraFactorExposures:
         years = []
         for file in self._files:
             file_arr = file.split("_")
-            if file_arr[0] == "exposures":
-                year = file_arr[1].split(".")[0]
+            if file_arr[0] == "spec":
+                year = file_arr[2].split(".")[0]
                 years.append(year)
 
         return years
@@ -44,40 +44,16 @@ class BarraFactorExposures:
         new_cols = list(map(lambda x: x.split(" ")[0], df.columns))
         df = df.rename({col: new_col for col, new_col in zip(df.columns, new_cols)})
 
-        # Split Combined colum into Barrid and Factor
-        df = (
-            df.with_columns(pl.col("Combined").str.split("/").alias("parts"))
-            .with_columns(
-                pl.col("parts").list.first().alias("Barrid"),
-                pl.col("parts").list.last().alias("Factor"),
-            )
-            .drop(["Combined", "parts"])
-        )
-
         # Melt date headers into a column
-        df = df.unpivot(index=["Barrid", "Factor"], variable_name="Date", value_name="Exposure")
+        df = df.unpivot(index="Barrid", variable_name="Date", value_name="SpecificRisk")
 
         # Cast date type
         df = df.with_columns(pl.col("Date").str.strptime(pl.Date).dt.date())
+
+        # Reorder columns
+        df = df.select(["Date", "Barrid", "SpecificRisk"])
 
         # Sort
         df = df.sort(by=["Barrid", "Date"])
 
         return df
-
-    # # Future implementation
-    # def download(self, redownload: bool = False):
-    #     years = range(1995, 2026)
-
-    #     dfs = []
-    #     for year in tqdm(years, desc="Downloading parquet files"):
-    #         df = pl.read_parquet(self._folder / f"exposures_{year}.parquet")
-    #         dfs.append(self.clean(df))
-
-    #     result = pl.concat(dfs)
-
-    #     self.db.create("BARRA_FACTOR_EXPOSURES", result)
-
-    # def load(self) -> pl.DataFrame:
-
-    #     return self.db.read("BARRA_FACTOR_EXPOSURES")
