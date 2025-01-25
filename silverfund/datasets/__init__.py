4c6295732c58b8378698a9c56d3cb6514253eb78
Andrew Hall <95375458+andrewhall1124@users.noreply.github.com>
2025-01-25 11:16:22 -0700

    Working barra specific return dataset (#14)

--- silverfund/datasets/__init__.py
+++ silverfund/datasets/__init__.py
@@ -2,6 +2,7 @@ from .barra_factor_covariances import BarraFactorCovariances
 from .barra_factor_exposures import BarraFactorExposures
 from .barra_returns import BarraReturns
 from .barra_risk_forecasts import BarraRiskForecasts
+from .barra_specific_returns import BarraSpecificReturns
 from .barra_specific_risk_forecast import BarraSpecificRiskForecast
 from .crsp_daily import CRSPDaily
 from .crsp_monthly import CRSPMonthly
@@ -18,6 +19,7 @@ __all__ = [
     "BarraFactorCovariances",
     "BarraSpecificRiskForecast",
     "BarraReturns",
+    "BarraSpecificReturns",
     "BarraRiskForecasts",
     "RussellConstituents",
     "SecurityMapping",
