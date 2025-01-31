b4834e12d4471e4aec6d1d4ca3e658881e877c77
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-01-31 08:54:29 -0700

    Adjusted optimal momentum strategy to use lagged barra data and have unit beta constraints.

--- silverfund/components/strategies/momentum_z_strategy.py
+++ silverfund/components/strategies/momentum_z_strategy.py
@@ -3,7 +3,7 @@ import polars as pl
 from silverfund.components.enums import Interval
 from silverfund.components.new_risk_model import NewRiskModel
 from silverfund.components.optimizers import qp
-from silverfund.components.optimizers.new_constraints import FullInvestment, NoLeverage
+from silverfund.components.optimizers.new_constraints import FullInvestment, UnitBeta
 from silverfund.components.strategies.strategy import Strategy
 
 
@@ -13,12 +13,8 @@ class MomentumZStrategy(Strategy):
         self._window = {Interval.DAILY: 252, Interval.MONTHLY: 13}[self._interval]
         self._rolling_window = {Interval.DAILY: 230, Interval.MONTHLY: 11}[self._interval]
         self._lag = {Interval.DAILY: 22, Interval.MONTHLY: 2}[self._interval]
-        self._barra_lag = {Interval.DAILY: 2, Interval.MONTHLY: 1}[self._interval]
 
     def compute_signal(self, chunk: pl.DataFrame) -> pl.DataFrame:
-        self._t = chunk["date"].unique().sort()[-1]
-        self._t_lag = chunk["date"].unique().sort()[-self._barra_lag - 1]
-
         # Momentum signal formation
         chunk = chunk.with_columns(pl.col("ret").log1p().alias("logret")).with_columns(
             pl.col("logret")
@@ -30,13 +26,18 @@ class MomentumZStrategy(Strategy):
         )
 
         # Momentum lag/skip
-        chunk = chunk.with_columns(pl.col("mom").shift(self._lag).over("barrid"))
+        chunk = chunk.with_columns(pl.col("mom").shift(self._lag).over("barrid").alias("mom_lag"))
 
         # Lag total_risk
-        chunk = chunk.with_columns(pl.col("total_risk").shift(self._barra_lag).over("barrid"))
+        chunk = chunk.with_columns(
+            pl.col("total_risk").shift(1).over("barrid").alias("total_risk_lag")
+        )
+
+        # Lag predbeta
+        chunk = chunk.with_columns(pl.col("predbeta").shift(1).over("barrid").alias("predbeta_lag"))
 
         # Non-null momentum
-        chunk = chunk.drop_nulls(subset="mom")
+        chunk = chunk.drop_nulls(subset="mom_lag")
 
         return chunk
 
@@ -45,7 +46,9 @@ class MomentumZStrategy(Strategy):
 
         # Z-score the momentum signal
         chunk = chunk.with_columns(
-            ((pl.col("mom") - pl.col("mom").mean()) / pl.col("mom").std()).alias("score")
+            ((pl.col("mom_lag") - pl.col("mom_lag").mean()) / pl.col("mom_lag").std()).alias(
+                "score"
+            )
         )
 
         return chunk
@@ -55,30 +58,25 @@ class MomentumZStrategy(Strategy):
 
         ic = 0.05
 
-        chunk = chunk.with_columns((pl.col("total_risk") * pl.col("mom") * ic).alias("alpha"))
+        chunk = chunk.with_columns(
+            (pl.col("total_risk_lag") * pl.col("mom_lag") * ic).alias("alpha")
+        )
 
         return chunk
 
     def compute_portfolio(self, chunk: pl.DataFrame) -> list[pl.DataFrame]:
         chunk = self.compute_alpha(chunk)
+        date_lag = chunk["date_lag_1d"].max()
         barrids = chunk["barrid"].unique().to_list()
 
-        # Load
-        covariance_matrix = NewRiskModel(self._t_lag, barrids).load()
-        alphas = chunk.select(["barrid", "alpha"])
-
-        # Convert to numpy matrix and vecotr
+        # Load covariance matrix on prior date
+        covariance_matrix = NewRiskModel(date_lag, barrids).load()
         covariance_matrix = covariance_matrix.drop("barrid").to_numpy()
-        alphas = alphas["alpha"].to_numpy()
 
         # Optimize
-        constraints = [FullInvestment, NoLeverage]
-        weights = qp(alphas, covariance_matrix, constraints)
+        constraints = [FullInvestment, UnitBeta]
+        portfolio = qp(chunk, covariance_matrix, constraints)
 
-        # Package portfolio
-        portfolio = chunk.with_columns(pl.Series(weights).alias("weight")).select(
-            ["date", "barrid", "weight"]
-        )
         return portfolio
 
     @property
