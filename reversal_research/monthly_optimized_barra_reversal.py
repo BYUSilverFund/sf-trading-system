618220684b3e921a16503967cecd61f7c6a51e8f
brandonwaits12 <brandonwaits12@gmail.com>
2025-02-02 22:26:44 -0700

    help

--- reversal_research/monthly_optimized_barra_reversal.py
+++ reversal_research/monthly_optimized_barra_reversal.py
@@ -6,11 +6,15 @@ import seaborn as sns
 from silverfund.backtester import Backtester
 from silverfund.components.enums import Interval
 from silverfund.components.strategies.reversal_z_strategy import ReversalZStrategy
-from silverfund.datasets import MasterMonthly
+from silverfund.datasets.master_monthly import MasterMonthly
 
 # Monthly backtest
-start_date = date(1995, 7, 31)  # Russell is missing 2012-07, and a day early 2017-11-29
+# start_date = date(1995, 7, 31)
+start_date = date(1995, 7, 31)
 end_date = date(2024, 12, 31)
+
+# start_date = date(2024, 10, 31)
+# end_date = date(2024, 12, 31)
 
 # Load historical dataset
 historical_data = (
@@ -19,11 +23,30 @@ historical_data = (
         end_date=end_date,
     )
     .load_all()
-    .select(["date", "barrid", "mktcap", "div_yield", "price", "ret", "total_risk", "spec_risk", "log_spec_ret"])
+    .select(
+        [
+            "date",
+            "barrid",
+            "mktcap",
+            "div_yield",
+            "price",
+            "ret",
+            "total_risk",
+            "spec_risk",
+            "log_spec_ret",
+        ]
+    )
 )
 
 # Create backtest instance
-bt = Backtester(start_date=start_date, end_date=end_date, interval=Interval.MONTHLY, historical_data=historical_data, strategy=MomentumZStrategy, security_identifier="barrid")
+bt = Backtester(
+    start_date=start_date,
+    end_date=end_date,
+    interval=Interval.MONTHLY,
+    historical_data=historical_data,
+    strategy=ReversalZStrategy,
+    security_identifier="barrid",
+)
 
 # Run backtest
 pnl = bt.run()
@@ -37,6 +60,8 @@ max_date = pnl["date"].max().strftime("%Y-%m-%d")
 print(f"From {min_date} to {max_date}")
 print(pnl)
 
-results_folder = "/Users/bwaits/Research/sf-trading-system/reversal_research"
+# results_folder = "/Users/bwaits/Research/sf-trading-system/reversal_research"
 
-pnl.write_parquet(f"{results_folder}/monthly_optimized_reversal_bt.parquet")
+pnl.write_parquet(
+    f"/home/bwaits/Research/sf-trading-system/reversal_research/monthly_optimized_reversal_bt.parquet"
+)
