4d203dcc6ee7962785e057826c09059cd30bfce1
Andrew Hall <95375458+andrewhall1124@users.noreply.github.com>
2025-02-03 14:16:01 -0700

    Added NoBuyingOnMargin and LeverageTarget constraints. (#23)
    * Added NoBuyingOnMargin and LeverageTarget constraints.
    
    * Removed LongOnly constraint since it is just a combination of other constraints.

--- silverfund/optimizers/new_constraints.py
+++ silverfund/optimizers/new_constraints.py
@@ -1,6 +1,7 @@
 from abc import ABC
 
 import cvxpy as cp
+import numpy as np
 
 
 class Constraint(ABC):
@@ -14,23 +15,15 @@ class FullInvestment(Constraint):
 
     @staticmethod
     def construct(**kwargs) -> list[cp.Constraint]:
-        weights = kwargs.get("weights")
+        weights: cp.Variable = kwargs.get("weights")
         return [cp.sum(weights) == 1]
-
-
-class NoLeverage(Constraint):
-
-    @staticmethod
-    def construct(**kwargs) -> list[cp.Constraint]:
-        weights = kwargs.get("weights")
-        return [weights >= -1, weights <= 1]
 
 
 class ZeroCost(Constraint):
 
     @staticmethod
     def construct(**kwargs) -> list[cp.Constraint]:
-        weights = kwargs.get("weights")
+        weights: cp.Variable = kwargs.get("weights")
         return [cp.sum(weights) == 0]
 
 
@@ -38,14 +31,40 @@ class UnitBeta(Constraint):
 
     @staticmethod
     def construct(**kwargs) -> list[cp.Constraint]:
-        weights = kwargs.get("weights")
-        betas = kwargs.get("betas")
-        return [cp.sum(cp.multiply(weights, betas)) == 1]
+        weights: cp.Variable = kwargs.get("weights")
+        predicted_betas: np.array = kwargs.get("betas")
+        return [cp.sum(cp.multiply(weights, predicted_betas)) == 1]
 
 
 class LongOnly(Constraint):
 
     @staticmethod
     def construct(**kwargs) -> list[cp.Constraint]:
-        weights = kwargs.get("weights")
+        weights: cp.Variable = kwargs.get("weights")
         return [weights >= 0]
+
+
+class NoBuyingOnMargin(Constraint):
+
+    @staticmethod
+    def construct(**kwargs) -> list[cp.Constraint]:
+        weights: cp.Variable = kwargs.get("weights")
+        return [weights <= 1]
+
+
+class ShortingLimit(Constraint):
+
+    @staticmethod
+    def construct(**kwargs) -> list[cp.Constraint]:
+        weights: cp.Variable = kwargs.get("weights")
+        return [weights >= -1]
+
+
+class LeverageTarget(Constraint):
+
+    @staticmethod
+    def construct(**kwargs) -> list[cp.Constraint]:
+        weights: cp.Variable = kwargs.get("weights")
+        bmk_weights: np.array = kwargs.get("bmk_weights")
+        active_leverage_target: float = kwargs.get("active_leverage_target")
+        return [cp.sum(weights - bmk_weights) == active_leverage_target]
