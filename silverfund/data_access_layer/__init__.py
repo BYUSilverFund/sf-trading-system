107287d5d13a993933a70db51094b8a7643076f1
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-02-10 14:45:01 -0700

    Working dal module.

--- silverfund/data_access_layer/__init__.py
+++ silverfund/data_access_layer/__init__.py
@@ -1,3 +1,6 @@
 """Module for data access layer"""
 
 from .trading_days import load_trading_days
+from .universe import load_monthly_universe
+
+__all__ = ["load_trading_days", "load_monthly_universe"]
--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from datetime import date
 import silverfund.data_access_layer as dal
 from silverfund.enums import Interval
 
-df = dal.load_trading_days(
-    interval=Interval.MONTHLY,
+df = dal.load_monthly_universe(
     start_date=date(1995, 7, 31),
     end_date=date(2024, 12, 31),
 )
