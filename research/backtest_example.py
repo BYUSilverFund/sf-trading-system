047875741260dd68ee4c3b0e35a8d5e313e186f1
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-02-16 11:03:19 -0600

    Research directory changes.

--- research/backtest_example.py
+++ research/backtest_example.py
@@ -9,25 +9,26 @@ from silverfund.scores import z_score
 from silverfund.signals import momentum
 from silverfund.strategies import Strategy
 
-# Date range
-start_date = date(2023, 1, 1)
-end_date = date(2024, 12, 31)
+if __name__ == "__main__":
+    # Date range
+    start_date = date(1999, 7, 1)
+    end_date = date(2024, 12, 31)
 
-# Define strategy
-strategy = Strategy(
-    signal_constructor=momentum,
-    score_constructor=partial(z_score, signal_col="mom"),
-    alpha_constructor=grindold_kahn,
-    portfolio_constructor=mean_variance_efficient,
-    constraints=[full_investment, no_buying_on_margin, long_only, unit_beta],
-)
+    # Define strategy
+    strategy = Strategy(
+        signal_constructor=momentum,
+        score_constructor=partial(z_score, signal_col="mom"),
+        alpha_constructor=grindold_kahn,
+        portfolio_constructor=mean_variance_efficient,
+        constraints=[full_investment, no_buying_on_margin, long_only, unit_beta],
+    )
 
-# Instantiate backtester
-bt = Backtester(start_date=start_date, end_date=end_date, strategy=strategy)
+    # Instantiate backtester
+    bt = Backtester(start_date=start_date, end_date=end_date, strategy=strategy)
 
-# Run sequentially
-pnl = bt.run_sequential()
-print("-" * 20 + " Asset Returns " + "-" * 20)
-print(pnl)
+    # Run sequentially
+    pnl = bt.run_sequential()
+    print("-" * 20 + " Asset Returns " + "-" * 20)
+    print(pnl)
 
-pnl.write_parquet("research/backtest_example.parquet")
+    pnl.write_parquet("research/backtest_example.parquet")
