c1adc96fbb38ae9a9c61093255ae972d63fd13cd
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-02-17 16:38:52 -0700

    Fixed backtest example plot.

--- research/backtest_example_plot.py
+++ research/backtest_example_plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import polars as pl
 import seaborn as sns
 
@@ -6,22 +7,31 @@ import seaborn as sns
 pnl = pl.read_parquet("research/backtest_example.parquet")
 
 # Aggregate to portfolio level
-daily_returns = (
-    pnl.with_columns((pl.col("weight") * pl.col("fwd_ret")).alias("contribution"))
+portfolio_returns = (
+    pnl.with_columns((pl.col("weight") * pl.col("fwd_ret").fill_null(0)).alias("contribution"))
     .group_by("date")
-    .agg(pl.col("contribution").sum().alias("portfolio_ret"))
+    .agg(
+        pl.col("contribution").sum().alias("portfolio_ret"),
+        pl.col("barrid").count().alias("n_assets"),
+    )
     .sort("date")
     .with_columns((pl.col("portfolio_ret") / 100).log1p().cum_sum().alias("portfolio_cumret"))
     .with_columns(pl.col("portfolio_cumret") * 100)  # put into percent space
 )
 
+# Sharpe
+portfolio_ret = portfolio_returns["portfolio_ret"].mean()
+portfolio_vol = portfolio_returns["portfolio_ret"].std()
+portfolio_sharpe = (portfolio_ret / portfolio_vol) * np.sqrt(12)
+print(f"Sharpe: {portfolio_sharpe:.4f}")
+
 # Table
 print("-" * 20 + " Portfolio Returns " + "-" * 20)
-print(daily_returns)
+print(portfolio_returns)
 
 # Chart
 plt.figure(figsize=(10, 6))
-sns.lineplot(data=daily_returns, x="date", y="portfolio_cumret")
+sns.lineplot(data=portfolio_returns, x="date", y="portfolio_cumret")
 plt.title("Monthly Momentum Backtest")
 plt.xlabel(None)
 plt.ylabel("Cummulative Sum Returns (%)")
