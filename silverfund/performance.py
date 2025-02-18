d5a286eeb2a39f0a812c3a678a8ad4870352d47b
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-02-18 14:47:44 -0700

    Added title to performance plots. Updated example code. Added momentum results.

--- silverfund/performance.py
+++ silverfund/performance.py
@@ -55,7 +55,11 @@ class Performance:
         self._periods = self._portfolio_returns["date"].unique().count()
 
     def plot_returns(
-        self, compounding: Compounding, decompose: bool = False, save_file_path: str | None = None
+        self,
+        compounding: Compounding,
+        title: str,
+        decompose: bool = False,
+        save_file_path: str | None = None,
     ) -> None:
         df = self._portfolio_returns
 
@@ -114,6 +118,7 @@ class Performance:
             sns.lineplot(df, x="date", y="active_ret", label="Active")
             plt.legend()
 
+        plt.title(title)
         plt.xlabel(None)
         plt.ylabel(f"Cummulative {compounding.value.title()} Returns (%)")
         plt.grid()
