f35c5016eee26454cb1285b52e89c28709e7cedf
jai-chau <jaichauchau@gmail.com>
2025-02-24 00:51:06 -0700

    Zero Beta Backtest py file

--- research/betting_against_beta/barra_beta/barraBAB_zerobeta.py
+++ research/betting_against_beta/barra_beta/barraBAB_zerobeta.py
@@ -1,7 +1,5 @@
-from multiprocessing import Pool
 from datetime import date
 from functools import partial
-import pandas as pd
 
 from silverfund.alphas import grindold_kahn
 from silverfund.backtester import Backtester
@@ -12,10 +10,13 @@ from silverfund.scores import z_score
 from silverfund.signals import barraBAB
 from silverfund.strategies import Strategy
 
-def run_backtest_for_year(year: int, interval: Interval, start_date: date, end_date: date):
-    year_start_date = date(year, 1, 1)
-    year_end_date = date(year, 12, 31)
+if __name__ == "__main__":
+    # Date range
+    start_date = date(1996, 1, 1)
+    end_date = date(2023, 12, 31)
+    interval = Interval.DAILY
 
+    # Define strategy
     strategy = Strategy(
         signal_constructor=barraBAB,
         score_constructor=partial(z_score, signal_col="predbeta"),
@@ -27,14 +28,15 @@ def run_backtest_for_year(year: int, interval: Interval, start_date: date, end_d
     )
 
     universe = dal.load_universe(interval=interval, start_date=start_date, end_date=end_date)
+
     training_data = universe.join(
-        dal.load_barra_returns(interval=interval, start_date=year_start_date, end_date=year_end_date),
+        dal.load_barra_returns(interval=interval, start_date=start_date, end_date=end_date),
         on=["date", "barrid"],
         how="left",
     ).sort(["barrid", "date"])
     
     barra_total_risk = dal.load_total_risk(
-        interval=interval, start_date=year_start_date, end_date=year_end_date, quiet=False
+        interval=interval, start_date=start_date, end_date=end_date, quiet=False
     )
     
     training_data = training_data.join(
@@ -43,22 +45,12 @@ def run_backtest_for_year(year: int, interval: Interval, start_date: date, end_d
         how="left"
     )
 
-    bt = Backtester(interval=interval, start_date=year_start_date, end_date=year_end_date, data=training_data)
+    # Instantiate backtester
+    bt = Backtester(interval=interval, start_date=start_date, end_date=end_date, data=training_data)
 
+    # Run sequentially
     pnl = bt.run_sequential(strategy)
-    print(f"Backtest completed for {year}")
-    
-    pnl.write_parquet(f"results/barraBAB_backtest_{year}.parquet")
+    print("-" * 20 + " Asset Returns " + "-" * 20)
+    print(pnl)
 
-def run_parallel_backtests():
-    # Date range
-    start_date = date(1996, 1, 1)
-    end_date = date(2023, 12, 31)
-    interval = Interval.DAILY
-
-    with Pool() as pool:
-        years = range(start_date.year, end_date.year + 1)
-        pool.starmap(run_backtest_for_year, [(year, interval, start_date, end_date) for year in years])
-
-if __name__ == "__main__":
-    run_parallel_backtests()
+    pnl.write_parquet("barraBAB_backtest.parquet")
