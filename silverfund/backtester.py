8855421e99a519ac0e0ba95d102fe827a9a4adc9
Andrew Hall <95375458+andrewhall1124@users.noreply.github.com>
2025-02-23 22:09:09 -0700

    Feature/documentation (#31)
    * Added documentation to the data access layer.
    
    * Added documentation for the entire code base.

--- silverfund/backtester.py
+++ silverfund/backtester.py
@@ -13,13 +13,42 @@ from silverfund.strategies import Strategy
 
 
 class Backtester:
+    """
+    A backtesting class that calculates alpha signals, constructs portfolios, and computes forward returns
+    for a given strategy within a specified time interval.
+
+    Attributes:
+        _interval (Interval): The time interval for the backtest.
+        _start_date (date): The start date of the backtest.
+        _end_date (date): The end date of the backtest.
+        _data (pl.DataFrame): The data to be used for the backtest, in the form of a Polars DataFrame.
+    """
+
     def __init__(self, interval: Interval, start_date: date, end_date: date, data: pl.DataFrame):
+        """
+        Initializes a Backtester instance.
+
+        Args:
+            interval (Interval): The time interval for the backtest.
+            start_date (date): The start date of the backtest.
+            end_date (date): The end date of the backtest.
+            data (pl.DataFrame): The data for the backtest.
+        """
         self._interval = interval
         self._start_date = start_date
         self._end_date = end_date
         self._data = data
 
     def _compute_alphas(self, strategy: Strategy) -> Alpha:
+        """
+        Computes the alphas for a given strategy based on signals and scores.
+
+        Args:
+            strategy (Strategy): The strategy object used to generate signals, scores, and alphas.
+
+        Returns:
+            Alpha: A record containing the computed alpha values.
+        """
         # Calculate signals, scores, and alphas
         signals = strategy.signal_constructor(self._data)
         scores = strategy.score_constructor(signals)
@@ -28,6 +57,15 @@ class Backtester:
         return Alpha(alphas)
 
     def _compute_forward_returns(self, portfolios: list[Portfolio]) -> AssetReturns:
+        """
+        Computes forward returns for a list of portfolios.
+
+        Args:
+            portfolios (list[Portfolio]): A list of portfolios to compute forward returns for.
+
+        Returns:
+            AssetReturns: A record containing the computed asset returns.
+        """
         # Getting forward returns
         testing_data = (
             self._data.with_columns(pl.col("ret").shift(-1).over("barrid").alias("fwd_ret"))
@@ -45,6 +83,16 @@ class Backtester:
         return AssetReturns(asset_returns)
 
     def run_sequential(self, strategy: Strategy) -> AssetReturns:
+        """
+        Runs the backtest sequentially by computing alphas, constructing portfolios,
+        and calculating forward returns.
+
+        Args:
+            strategy (Strategy): The strategy object used for portfolio construction and signal generation.
+
+        Returns:
+            AssetReturns: A record containing the computed asset returns.
+        """
         # Get universe
         universe = dal.load_universe(
             interval=self._interval,
@@ -71,6 +119,19 @@ class Backtester:
     def construct_portfolio(
         period, universe, alphas, strategy, progress_bar: tqdm_ray.tqdm | None = None
     ):
+        """
+        Constructs a portfolio for a specific period using a given strategy.
+
+        Args:
+            period (str): The date period for which the portfolio is being constructed.
+            universe (pl.DataFrame): The universe of available assets for portfolio construction.
+            alphas (Alpha): The computed alphas used for portfolio construction.
+            strategy (Strategy): The strategy used to construct the portfolio.
+            progress_bar (tqdm_ray.tqdm, optional): A Ray-based progress bar for tracking progress.
+
+        Returns:
+            Portfolio: A constructed portfolio for the given period.
+        """
         # Get portfolio constructor parameters
         period_barrids = universe.filter(pl.col("date") == period)["barrid"].sort().to_list()
         period_alphas = Alpha(alphas.filter(pl.col("date") == period).sort(["barrid"]))
@@ -90,6 +151,17 @@ class Backtester:
         return portfolio
 
     def run_parallel(self, strategy: Strategy, n_cpus: int | None = None) -> AssetReturns:
+        """
+        Runs the backtest in parallel by computing alphas, constructing portfolios,
+        and calculating forward returns using multiple CPU cores.
+
+        Args:
+            strategy (Strategy): The strategy object used for portfolio construction and signal generation.
+            n_cpus (int, optional): The number of CPU cores to use for parallel execution.
+
+        Returns:
+            AssetReturns: A record containing the computed asset returns.
+        """
         # Get universe
         universe = dal.load_universe(
             interval=self._interval,
