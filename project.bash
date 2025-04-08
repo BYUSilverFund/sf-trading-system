cad5e24d23904e11f220bd3168875c989fab6d73
MaxwellS <msch2022@byu.edu>
2025-04-08 13:34:05 -0600

    stuff

---
+++ project.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00   # walltime
+#SBATCH --ntasks=64   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=4096M   # memory per CPU core
+#SBATCH -J "project"   # job name
+#SBATCH --mail-user=msch2022@byu.edu   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python filename.py
